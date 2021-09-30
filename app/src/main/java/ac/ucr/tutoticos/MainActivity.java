d98c98130ab41082a5464b912829a4faf94fff57
Nixon-fj <nixon.fernandez@ucr.ac.cr>
2021-09-29 20:49:52 -0600

    Initial commit

---
+++ app/src/main/java/ac/ucr/tutoticos/MainActivity.java
@@ -0,0 +1,15 @@
+package ac.ucr.tutoticos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }//fin del onCreate
+
+}//fin de la clase
