6c6de9d0a1c95168d2e9d6af9b40f0a269ee33b2
Nixon-fj <nixon.fernandez@ucr.ac.cr>
2021-10-01 00:09:11 -0600

    Avance
    Primer avace de las vistas, perfiltutor e inicio de sesion

---
+++ app/src/main/java/ac/ucr/tutoticos/act_iniciosesion.java
@@ -0,0 +1,18 @@
+package ac.ucr.tutoticos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class act_iniciosesion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.lyt_iniciosesion);
+
+        //
+
+    }//fin del onCreate
+
+}//fin de la clase
