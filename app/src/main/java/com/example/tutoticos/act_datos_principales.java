7bd83ffd7b3114eb5781507c6ddca711b7c1b803
ange-ram <ange.ramlean@gmail.com>
2021-10-04 23:59:43 -0600

    Ventanas registro, perfil, datos principales

---
+++ app/src/main/java/com/example/tutoticos/act_datos_principales.java
@@ -0,0 +1,14 @@
+package com.example.tutoticos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class act_datos_principales extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.lyt_datos_principales);
+    }
+}
---
+++ app/src/main/java/com/example/tutoticos/act_registro.java
@@ -0,0 +1,29 @@
+package com.example.tutoticos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class act_registro extends AppCompatActivity {
+
+    Button siguiente;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.lyt_registro);
+
+        siguiente = findViewById(R.id.btnRegistrar);
+
+        siguiente.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(act_registro.this, act_tipo_usuario.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
