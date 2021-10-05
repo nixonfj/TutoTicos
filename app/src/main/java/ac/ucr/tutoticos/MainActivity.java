8de06658de80391d4dfa615bf9d25474fab3aea5
Nixon-fj <nixon.fernandez@ucr.ac.cr>
2021-10-05 14:24:36 -0600

    ajustes
    ajustes

--- app/src/main/java/ac/ucr/tutoticos/MainActivity.java
+++ app/src/main/java/ac/ucr/tutoticos/MainActivity.java
@@ -4,12 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.tutoticos.R;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.lyt_datos_principales);
     }//fin del onCreate
 
 }//fin de la clase
