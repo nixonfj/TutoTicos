4839ffde546dff75c0dca52b4e5ce3352a7346ee
shirleyzj16 <70995515+shirleyzj16@users.noreply.github.com>
2021-10-05 18:23:30 -0600

    pantallaPrincipal

---
+++ app/src/main/java/ac/ucr/tutoticos/act_pantallaprincipal.java
@@ -0,0 +1,14 @@
+package ac.ucr.tutoticos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class act_pantallaprincipal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.lyt_pantallaprincipal);
+    }
+}
