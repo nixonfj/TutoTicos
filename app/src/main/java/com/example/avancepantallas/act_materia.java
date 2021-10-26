ad972fb9f1053c847c5feeb8aab62176887f8fd0
ange-ram <ange.ramlean@gmail.com>
2021-10-26 06:16:51 -0600

    Avance pantallas

---
+++ app/src/main/java/com/example/avancepantallas/act_materia.java
@@ -0,0 +1,49 @@
+package com.example.avancepantallas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Spinner;
+
+public class act_materia extends AppCompatActivity {
+
+    private Button btnAddMateria;
+    private LinearLayout lytMateria;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.lyt_materia);
+
+        btnAddMateria = findViewById(R.id.btnAddMateria);
+        lytMateria = findViewById(R.id.lytMateria);
+
+        btnAddMateria.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addMateria();
+            }
+        });
+    }
+
+    private void addMateria(){
+        View nuevaVista = getLayoutInflater().inflate(R.layout.lyt_materia_perso, null,false);
+
+        Spinner spMateria;
+
+        spMateria = nuevaVista.findViewById(R.id.spMateria);
+
+
+        String [] opciones= {"Español", "Matemáticas", "Estudios Sociales", "Ciencias", "Civica", "Química", "Biología", "Física Mate"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.spinner_perso, opciones);
+        spMateria.setAdapter(adapter);
+
+        lytMateria.addView(nuevaVista);
+    }
+}
